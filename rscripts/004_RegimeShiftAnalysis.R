63153d1220261cd93b3a694f8045ad9a8786a64d
Nathan T. Barrus <nbarrus1@gmail.com>
2025-03-26 07:32:24 -0400

    ##perform the first regime shift detection: STARS: 3-25-2025

--- rscripts/004_RegimeShiftAnalysis.R
+++ rscripts/004_RegimeShiftAnalysis.R
@@ -13,31 +13,31 @@ theme_set(theme_bw())
 ###create the functions designed by Rodionov & Overland, Journal of Marine Science,
 ###2005
 
-RSI <- function() {}
 
-
-
-##load in the final set of time series for meta-analysis
-
-load(here("output","final_set.Rdata"))
-
-
-test.df <- final.set$ls[[1]] 
-
-l = 5
+STARS <- function(test.df, l = 5, p.crit=0.05, transform = FALSE) {
+  
 degrees.freedom = l*2 - 2
-p.crit = 0.05
 t = qt(p.crit/2, df = degrees.freedom, lower.tail = FALSE)
 
 
-y.seq = test.df |> 
+if (transform == FALSE) {
+  y.seq = test.df |> 
   ungroup() |> 
+  mutate(lamba.ln = log(lead(y)/y)) |> 
   pull(y) 
+  } else {
+    y.seq = test.df |> 
+      ungroup() |> 
+      mutate(y = if_else(y == 0, true = 0.001, false = y),
+        lamba.ln = log(lead(y)/y)) |> 
+      pull(lamba.ln)   
+    
+  }
 
 
 
 y.matrix <- matrix(0, nrow = length(y.seq), ncol = l)
-colnames(y.matrix) <- paste0("y",0:4)
+colnames(y.matrix) <- paste0("y",(0:(l-1)))
 rownames(y.matrix) <- paste0(test.df$x)
 
 for (i in 1:l) {
@@ -70,14 +70,16 @@ sd.l <- as_tibble(y.matrix) |>
 
 diff = t*sqrt(2*var.l/l)
 
-x.cur = mean(y.seq[1:l])
+x.cur = mean(y.seq[1:l], na.rm = T)
 crit.upp = x.cur + diff
 crit.low = x.cur - diff
 RSI = rep(0, times = l)
-REGIMESHIFT = data.frame(x = test.df$x,
-                        REGIME = rep(NA_character_, times = length(test.df$x)))
+test.df$REGIME <- NA_character_
+test.df$REGIME.AVE <- 0
+h = 1
 
-for (i in (l+1):length(y.seq)) {
+
+for (i in 2:length(y.seq)) {
   
 #-------------------------------------------------
 ##first if  if else statement is for determining the
@@ -87,16 +89,36 @@ for (i in (l+1):length(y.seq)) {
 ##if yes and is above the upper tail
 ###calculate RSI
   
+  if (is.na(y.seq[i])){
+    
+    test.df$REGIME[i] <- NA_character_
+    
+    x.cur = x.cur
+    
+    test.df$REGIME.AVE[h:i] <- NA_real_
+    
+    crit.upp = x.cur + diff
+    crit.low = x.cur - diff
+    
+    RSI = rep(0, times = l)
+    print(paste0(test.df$x[i],": NA"))
+    
+    
+  } else {
+  
   if (y.seq[i] >= crit.upp) {  
     
-    RSI[1] <- y.seq[i]-(x.cur+sd.l)
     
-   for(j in 2:l) {  
+    RSI[1] <- (y.seq[i]-(x.cur+diff))/(l*sd.l)
+    
+    for(j in 2:l) {  
       
-      RSI[j] <- (y.seq[i+(j-1)]-(x.cur+sd.l)) + sum(RSI)
-        
+      RSI[j] <- (y.seq[i+j-1]-(x.cur+diff))/(l*sd.l)
+      
     }
-  
+    
+    RSI <- cumsum(RSI)    
+    
     ##----------------
     ##second if else is used to evaluate the RSI
     ##----------------
@@ -106,23 +128,29 @@ for (i in (l+1):length(y.seq)) {
     
     if (any(RSI < 0, na.rm = T)) {
       
-      REGIMESHIFT$REGIME[i] <- "no"
+      test.df$REGIME[i] <- "no"
       
-      x.cur = mean(y.seq[(i-l):i],na.rm =T)
+      x.cur = mean(y.seq[if((i<=(h+l-1))){(h:(h+l-1))}else{h:i}],na.rm =T)
+      
+      test.df$REGIME.AVE[h:i] <- x.cur
+      
       crit.upp = x.cur + diff
       crit.low = x.cur - diff
       
       RSI = rep(0, times = l)
-      print(i)
+      print(paste0(test.df$x[i],": Negative RSI"))
       
       #if there were no negative values recalculate the x.cur using the 
       #the new regime
       
     } else { 
       
-      REGIMESHIFT$REGIME[i] <- "yes"
+      test.df$REGIME[i] <- "yes"
       
       x.cur = mean(y.seq[i:(i+l-1)],na.rm =T)
+      
+      test.df$REGIME.AVE[i] <- x.cur
+      
       crit.upp = x.cur + diff
       crit.low = x.cur - diff
       
@@ -130,7 +158,7 @@ for (i in (l+1):length(y.seq)) {
       
       h = i
       
-      print(i)
+      print(paste0(test.df$x[i],": Regime Shift"))
       
       
     }
@@ -142,13 +170,16 @@ for (i in (l+1):length(y.seq)) {
     
     ##calculate the RSI
     
-    RSI[1] <- (-y.seq[i]-l-sd.l)*-1
-    
-    for(j in 2:l) {
+      RSI[1] <- (x.cur-diff-y.seq[i])/(l*sd.l)
       
-      RSI[j] <- (-y.seq[i+(j-1)]-l-sd.l)*-1 + sum (RSI)
+      for(j in 2:l) {  
+        
+        RSI[j] <- (x.cur-diff-y.seq[i+j-1])/(l*sd.l)
+        
+      }
       
-    }
+      RSI <- cumsum(RSI)  
+      
     ##----------------
     ##second if else is used to evaluate the RSI
     ##----------------
@@ -158,23 +189,30 @@ for (i in (l+1):length(y.seq)) {
     
     if (any(RSI < 0, na.rm = T)) {
       
-      REGIMESHIFT$REGIME[i] <- "no"
+      test.df$REGIME[i] <- "no"
       
-      x.cur = mean(y.seq[(i-l):i],na.rm =T)
+      x.cur = mean(y.seq[if((i<=(h+l-1))){(h:(h+l-1))}else{h:i}],na.rm =T)
+      
+      test.df$REGIME.AVE[i] <- x.cur
+      
       crit.upp = x.cur + diff
       crit.low = x.cur - diff
       
       RSI = rep(0, times = l)
-      print(i)
+      print(paste0(test.df$x[i],": Negative RSI"))
+  
       
     #if there were no negative values recalculate the x.cur using the 
     #the new regime
       
     } else { 
       
-      REGIMESHIFT$REGIME[i] <- "yes"  
+      test.df$REGIME[i] <- "yes"  
       
     x.cur = mean(y.seq[i:(i+l-1)],na.rm =T)
+    
+    test.df$REGIME.AVE[i] <- x.cur
+    
     crit.upp = x.cur + diff
     crit.low = x.cur - diff
     
@@ -182,7 +220,7 @@ for (i in (l+1):length(y.seq)) {
     
     h = i
     
-    print(i) 
+    print(paste0(test.df$x[i],": Regime Shift"))
     
       }
     
@@ -194,26 +232,128 @@ for (i in (l+1):length(y.seq)) {
   }else{                         #if the next value doesn not change in critical value
                                  #then lump with previous sequence and update cur mean and critical values
     
-    REGIMESHIFT$REGIME[i] <- "no"
+    test.df$REGIME[i] <- "no"
     
-    if(is.na(REGIMESHIFT$REGIME[(i-1)]) | !any(REGIMESHIFT$REGIME == "yes", na.rm = T)) {
+    if(is.na(test.df$REGIME[(i-1)]) | !any(test.df$REGIME == "yes", na.rm = T)) {
     
-      x.cur = mean(y.seq[(i-l):i],na.rm =T)
+      x.cur = mean(if(i<=l){y.seq[1:l]}else{y.seq[1:i]},na.rm =T)
+      
+      test.df$REGIME.AVE[1:i] <- x.cur
+      
       crit.upp = x.cur + diff
       crit.low = x.cur - diff
-      print(i)
+      print(paste0(test.df$x[i]))
       
     } else {
       
-      x.cur = mean(y.seq[if((i<=(h+l-1))){(h:(h+l-1))}else{((h+(i-h)):(h+(i-h)+l-1))}],na.rm =T)
+      x.cur = mean(y.seq[if((i<=(h+l-1))){(h:(h+l-1))}else{h:i}],na.rm =T)
+      
+      test.df$REGIME.AVE[h:i] <- x.cur
+      
       crit.upp = x.cur + diff
       crit.low = x.cur - diff
       
       RSI = rep(0, times = l)  
-      print(i) 
+      print(paste0(test.df$x[i]))
       
     }
     
   } 
 }
+
+
+}
+
+test.df
+
+}
+
+
+#--------------------------------------------
+####apply STARS function to timeseries ####
+#--------------------------------------------
+
+##load in the final set of time series for meta-analysis
+
+load(here("output","final_set.Rdata"))
+
+
+final.set <- final.set |> 
+  mutate(regime.length = if_else(years.surveyed < 15, true = 3, false = 5),
+         ls = map2(.x = ls, .y = regime.length, .f = STARS, transform = FALSE))
+
+
+###
+
+
+
+plot_timeseries <- function(df) {
+  df |> 
+    ggplot(aes(x = x, y = y))+
+    geom_line()+
+    geom_point(size = 2, color = "black", shape = 21, fill = "#666666")+
+    #geom_line(aes(y = REGIME.AVE), color = "red", linetype = "dashed")+
+    geom_vline(data = df |> filter(REGIME == "yes"), aes(xintercept = x), color = "red", linetype = "dashed")+
+    theme_bw(base_size = 12) +
+    theme(
+      axis.ticks.length = unit(.25, "cm"),
+      axis.title.y = element_text(vjust = 2),
+      plot.subtitle = element_text(face = "italic", size = 9),
+      plot.title = element_text(size = 9),
+      axis.title = element_text(size = 8),
+      axis.text = element_text(size = 7)
+      #plot.margin = unit(c(0,0,0,0), "cm")
+    )+
+    labs(x = unique(df$x_variable), y = unique(df$measure),
+         subtitle = unique(df$species.names),
+         title = paste0(unique(df$author)))
+}
+
+
+
+final.plots <- final.set |>
+  unnest(cols = ls) |> 
+  group_by(plot,group) |> 
+  nest(.key = "ls") |> 
+  mutate(timeseries = map(ls, .f = plot_timeseries)) |> 
+  select(-ls)
+
+
+final.plots$timeseries[11:20]
+
+
+save(final.plots,file = here("output","literatrure_timeseries_plots.Rdata"))
+
+#---------------------------------------------
+###for loop to save my ggplot pannels###
+#---------------------------------------------
+
+
+nrow(final.plots)/12
+
+pdf(here("output","timeseriespanels.pdf"), width = 11, height = 8)
+
+
+for(i in 1:((nrow(final.plots)/12)-1)) {
+
+  
+if (i == 1) {
+  
+    timeseries.panel <- reduce(final.plots$timeseries[i:(i*12)],`+`)+
+      plot_layout(ncol = 4, nrow = 3)
+    print(timeseries.panel)
+    
+} else {
+  
+   timeseries.panel <- reduce(final.plots$timeseries[((i*12)+1):((i+1)*12)],`+`)+
+      plot_layout(ncol = 4, nrow = 3)
+   print(timeseries.panel)
+   
+}
+    
+}
+
+
+dev.off()
+
 
