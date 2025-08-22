47750754e5645da4eaa89244c2cab2ea03d70377
Nathan T. Barrus <nbarrus1@gmail.com>
2025-08-22 09:44:00 -0400

    Boom-Bust Classification Debug: 8-22-2025

--- rscripts/006_BoomBustClassification.R
+++ rscripts/006_BoomBustClassification.R
@@ -58,11 +58,15 @@ regime.means <- function(df.original, df.breaks) {
 
 
 
+df <- regimeclassification$predictions[[55]]
+n_regimes <- regimeclassification$brks_opt_num[[55]]
+
 classification_scheme <- function (df, n_regimes) {
   
   
   if(n_regimes > 0) {
-    
+  
+##calculate the maximum percent decline  
     decline_mag = df |> 
       ungroup() |> 
       group_by(regime.class) |> 
@@ -75,19 +79,52 @@ classification_scheme <- function (df, n_regimes) {
       summarise(perc_decline = max(perc_decline,na.rm = T)) |> 
       pull(perc_decline)
     
+##calculate the maximum rate of increase between the lag/growth and high n phase    
+     rate_increase <- df |> 
+       ungroup() |> 
+       mutate(lambda = lead(pred)/pred) |> 
+       group_by(regime.class) |> 
+       summarise(lambda.max = max(lambda, na.rm = T),
+                 lambda.min = min(lambda, na.rm = T),
+                 regime.max = mean(regime.max, na.rm =T),
+                 regime.min = mean(regime.min, na.rm = T)) |> 
+       ungroup() |> 
+       filter(lead(regime.max) > regime.max) |> 
+       pull(lambda.max)
+     
+     rate_increase <- if(is_empty(rate_increase)){NA_real_}else{max(rate_increase)}
+     
+##calculate the minimum rate of increase between the high n and low n phase      
+     rate_decline <- df |> 
+       ungroup() |> 
+       mutate(lambda = lead(pred)/pred) |> 
+       group_by(regime.class) |> 
+       summarise(lambda.max = max(lambda, na.rm = T),
+                 lambda.min = min(lambda, na.rm = T),
+                 regime.max = mean(regime.max, na.rm =T),
+                 regime.min = mean(regime.min, na.rm = T)) |> 
+       ungroup() |> 
+       filter(lag(regime.max) > regime.max) |> 
+       pull(lambda.min)
+     
+     rate_decline <- if(is_empty(rate_decline)){NA_real_}else{min(rate_decline)}
+##find the number of observation within the low n phase
     n_length <- df |> 
       group_by(regime.class) |> 
       summarise(n = n()) |>
       slice(length(unique(df$regime.class))) |> 
       pull(n) 
     
+##find the number of leads needed for the sustained calculation    
     n_leads = n_length-1
-    
-    n_sustained <- df |>
+
+##calculate the number of consecutive values across the sequence of the low n phase
+##that are  >= 90% smaller than the maximum
+   n_sustained <- max(df |>
       filter(regime.class == unique(df$regime.class)[length(unique(df$regime.class))]) |> 
       ungroup() |> 
       select(pred) |> 
-      mutate(pred = if_else(pred < max(df$pred)*0.1, true = 1, false = 0)) |> 
+      mutate(pred = if_else(pred <= max(df$pred)*0.1, true = 1, false = 0)) |> 
       mutate(across(
         everything(),
         .fns = list(!!!setNames(
@@ -98,14 +135,54 @@ classification_scheme <- function (df, n_regimes) {
       pivot_longer(cols = 1:(n_leads+1)) |> 
       group_by(nrow) |> 
       summarise(n_below = sum(value,na.rm = T)) |> 
-      pull(n_below)
-    
-    
+      pull(n_below))
+
+   
+##determine if the last value of the sequence in the low n phase is >= 90% smaller than
+## the max value
+
+decline_lastposition <- df|> 
+  filter(regime.class == unique(df$regime.class)[length(unique(df$regime.class))]) |> 
+  ungroup() |> 
+  select(pred) |> 
+  mutate(if.declined = if_else(pred <= max(df$pred)*0.1, true = 1, false = 0)) |> 
+  slice(n_length) |> 
+  pull(if.declined)
+
+
+###perform the classification using our definition    
     case_when(decline_mag == (-Inf)~"\nestablished",
-              decline_mag>=0.9 & max(n_sustained)>=3&length(unique(df$regime.class))==3 ~ "boom &\nbust",
-              decline_mag>=0.9 & !(max(n_sustained)>=3)&length(unique(df$regime.class))==3 ~ "boom &\nnot sust.",
-              decline_mag>=0.9 & max(n_sustained)>=3&length(unique(df$regime.class))==2 ~ "unk rate &\nbust",
-              decline_mag>=0.9 & !(max(n_sustained)>=3)&length(unique(df$regime.class))==2~ "unk rate &\nnot sust.",
+              decline_mag>=0.9 &
+                n_sustained>=3&
+                rate_increase >= 2&
+                rate_decline <= 0.5&
+                length(unique(df$regime.class))==3 ~ "boom &\nbust",
+              decline_mag>=0.9 &
+                !(n_sustained>=3)&
+                rate_increase >= 2&
+                rate_decline <= 0.5&
+                decline_lastposition > 0 &
+                length(unique(df$regime.class))==3 ~ "boom &\nnot sust.",
+              decline_mag>=0.9 &
+                n_sustained>=3&
+                rate_decline <= 0.5&
+                length(unique(df$regime.class))>=2 ~ "unk rate &\nbust",
+              decline_mag>=0.9 &
+                !(n_sustained>=3)&
+                rate_decline <= 0.5&
+                decline_lastposition > 0 &
+                length(unique(df$regime.class))>=2~ "unk rate &\nnot sust.",
+              decline_mag>=0.9 &
+                !(n_sustained>=3)&
+                rate_increase >= 2&
+                rate_decline <= 0.5&
+                !(decline_lastposition > 0) &
+                length(unique(df$regime.class))==3 ~ "\novershoot",
+              decline_mag>=0.9 &
+                !(n_sustained>=3)&
+                rate_decline <= 0.5&
+                !(decline_lastposition) > 0 &
+                length(unique(df$regime.class))>=2~ "\novershoot",
               decline_mag<0.9~"\novershoot")
     
   } else {
@@ -132,6 +209,10 @@ regimeclassification <- final.set.preds.brks |>
 
 
 save(regimeclassification, file = here("output","regimeclassification.Rdata"))
+
+
+table(regimeclassification$brks_opt_num>0,
+      is.na(regimeclassification$class))
 
 
 ###reading list
